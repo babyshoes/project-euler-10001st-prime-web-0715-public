3b6ee919ffe95adca2e397fb9675a23989a08a6c
babyshoes <dianecai@gmail.com>
2015-10-14 13:55:03 -0400

    done

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,15 @@
 # Implement your procedural solution here!
+require 'pry'
+def prime_number_for(num_of_elements)
+  arr = [2, 3]
+  shovel_primes(arr, num_of_elements)
+  arr[num_of_elements - 1]
+end
+
+def shovel_primes(arr, num_of_elements)
+  i ||= arr.last + 2
+  until arr.length == num_of_elements
+    arr << i unless arr.find{ |e| i % e == 0}
+    i += 2
+  end
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,23 @@
 # Implement your object-oriented solution here!
+class Prime
+  attr_reader :num_of_elements, :arr_of_primes
+
+  def initialize(num_of_elements)
+    @num_of_elements = num_of_elements
+    @arr_of_primes = [2, 3]
+  end
+
+  def number
+    set_arr_of_primes
+    @arr_of_primes[num_of_elements - 1]
+  end
+
+  def set_arr_of_primes
+    i = @arr_of_primes.last + 2
+    until @arr_of_primes.length == num_of_elements
+      @arr_of_primes << i unless @arr_of_primes.find{ |e| i % e == 0}
+      i += 2
+    end
+  end
+
+end
